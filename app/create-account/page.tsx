486939b82e0925b29b28f0278c2e989a6c3d3649
HannesCho <hoonsungcho11@gmail.com>
2024-04-01 20:34:05 +0200

    style: add style to creat-account page

--- app/create-account/page.tsx
+++ app/create-account/page.tsx
@@ -1,23 +1,39 @@
+import { ChatBubbleOvalLeftEllipsisIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
 
 export default function CreateAccount() {
   return (
-    <div>
-      <div>
-        <h1>Hallo!</h1>
-        <h2>Untenstehendes Formular ausfüllen, um beizutreten</h2>
+    <div className="flex flex-col gap-10 *:font-medium px-10 py-8">
+      <div className="*:font-medium">
+        <h1 className="text-3xl">Hallo!</h1>
+        <h2 className="text-2xl">
+          Untenstehendes Formular ausfüllen, um beizutreten
+        </h2>
       </div>
-      <form>
-        <div>
-          <input type="text" placeholder="Benutzername" required />
-          <span>Eingabefehler</span>
+      <form className="flex flex-col gap-3">
+        <div className="flex flex-col gap-3">
+          <input
+            type="text"
+            placeholder="Benutzername"
+            required
+            className="p-2 bg-transparent rounded-md outline-none ring-1 ring-red-300 focus:ring-2 focus:ring-red-400 focus:rounded-md"
+          />
+          <span className="text-sm text-red-700">Eingabefehler</span>
         </div>
-        <button>Konto erstellen</button>
+        <button className="w-full h-8 bg-red-500 rounded-md hover:bg-red-400">
+          Konto erstellen
+        </button>
       </form>
       <div>
-        <Link href="/sms"></Link>
-        <span>Symbol</span>
-        <span>Per SMS anmelden</span>
+        <Link
+          href="/sms"
+          className="flex flex-row items-center justify-center gap-3 text-white primary-btn"
+        >
+          <span>
+            <ChatBubbleOvalLeftEllipsisIcon className="size-6" />
+          </span>
+          <span>Per SMS anmelden</span>
+        </Link>
       </div>
     </div>
   );
