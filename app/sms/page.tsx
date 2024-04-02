b59e07aeedcf51f349f74ad987d15db4e533a5bb
HannesCho <hoonsungcho11@gmail.com>
2024-04-02 13:01:27 +0200

    feat: create the sms page
    - contains basic structure

---
+++ app/sms/page.tsx
@@ -0,0 +1,23 @@
+import Input from "../../components/input";
+import FormButton from "@/components/form-btn";
+
+export default function SMSLogin() {
+  return (
+    <div className="flex flex-col gap-10 *:font-medium px-10 py-8">
+      <div className="*:font-medium">
+        <h1 className="text-3xl">SMS-Anmeldung</h1>
+        <h2 className="text-2xl">Überprüfen Sie Ihre Telefonnummer</h2>
+      </div>
+      <form className="flex flex-col gap-3">
+        <Input type="text" placeholder="Telefonnummer" required errors={[]} />
+        <Input
+          type="number"
+          placeholder="Verifizierungscode"
+          required
+          errors={[]}
+        />
+        <FormButton loading={false} text="Überprüfen" />
+      </form>
+    </div>
+  );
+}
