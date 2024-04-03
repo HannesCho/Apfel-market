09c82474c0098d7d126ed816f2890ee72a182992
HannesCho <hoonsungcho11@gmail.com>
2024-04-03 21:32:18 +0200

    refactor: add config for reapeated variables

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,11 @@
 import createMiddleware from "next-intl/middleware";
+import { locales, defaultLocale } from "./config";
 
 export default createMiddleware({
-  // A list of all locales that are supported
-  locales: ["en", "de"],
-
-  // Used when no locale matches
-  defaultLocale: "en",
+  locales,
+  defaultLocale,
 });
 
 export const config = {
-  // Match only internationalized pathnames
   matcher: ["/", "/(de|en)/:path*"],
 };
