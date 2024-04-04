4f10ab19e993717f5a810c9eb434108e39a6a059
HannesCho <hoonsungcho11@gmail.com>
2024-04-04 19:10:21 +0200

    feat: add multi language to login page

--- src/app/[locale]/login/page.tsx
+++ src/app/[locale]/login/page.tsx
@@ -3,34 +3,28 @@ import FormButton from "@/components/form-btn";
 import SnsButton from "@/components/sns-btn";
 import { FaGithub } from "react-icons/fa";
 import { IoChatbubbleEllipsesOutline } from "react-icons/io5";
+import { useTranslations } from "next-intl";
 import { unstable_setRequestLocale } from "next-intl/server";
 
 export default function Login(props: { params: { locale: string } }) {
   unstable_setRequestLocale(props.params.locale);
+  const t = useTranslations("Login");
   return (
     <div className="flex flex-col gap-10 *:font-medium px-10 py-8">
       <div className="*:font-medium">
-        <h1 className="text-3xl">Hallo!</h1>
-        <h2 className="text-2xl">Mit E-Mail und Passwort anmelden</h2>
+        <h1 className="text-3xl">{t("h1")}</h1>
+        <h2 className="text-2xl">{t("h2")}</h2>
       </div>
       <form className="flex flex-col gap-3">
-        <Input type="email" placeholder="Email" required errors={[]} />
-        <Input type="password" placeholder="Passwort" required errors={[]} />
-        <FormButton loading={false} text="Anmelden" />
+        <Input type="email" placeholder={t("email")} required errors={[]} />
+        <Input
+          type="password"
+          placeholder={t("password")}
+          required
+          errors={[]}
+        />
+        <FormButton loading={false} text={t("login")} />
       </form>
-      <div className="w-full h-[1px] bg-slate-500" />
-      <div className="flex flex-col gap-3">
-        <SnsButton
-          href="/sms"
-          text="Per SMS anmelden"
-          Icon={IoChatbubbleEllipsesOutline}
-        />
-        <SnsButton
-          href="/github/start"
-          text="Per Github anmelden"
-          Icon={FaGithub}
-        />
-      </div>
     </div>
   );
 }
