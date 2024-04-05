c8fc3d475e82a1d3b8e64939ad19f2d2191d4add
HannesCho <hoonsungcho11@gmail.com>
2024-04-05 16:05:34 +0200

    feat: add multi language to sms login page

--- src/app/[locale]/sms/page.tsx
+++ src/app/[locale]/sms/page.tsx
@@ -1,24 +1,27 @@
 import Input from "../../../../components/input";
 import FormButton from "@/components/form-btn";
+import { useTranslations } from "next-intl";
 import { unstable_setRequestLocale } from "next-intl/server";
 
 export default function SMSLogin(props: { params: { locale: string } }) {
   unstable_setRequestLocale(props.params.locale);
+  const t = useTranslations("SMS");
+
   return (
     <div className="flex flex-col gap-10 *:font-medium px-10 py-8">
       <div className="*:font-medium">
-        <h1 className="text-3xl">SMS-Anmeldung</h1>
-        <h2 className="text-2xl">Überprüfen Sie Ihre Telefonnummer</h2>
+        <h1 className="text-3xl">{t("h1")}</h1>
+        <h2 className="text-2xl">{t("h2")}</h2>
       </div>
       <form className="flex flex-col gap-3">
-        <Input type="text" placeholder="Telefonnummer" required errors={[]} />
+        <Input type="text" placeholder={t("telephoe")} required errors={[]} />
         <Input
           type="number"
-          placeholder="Verifizierungscode"
+          placeholder={t("verification")}
           required
           errors={[]}
         />
-        <FormButton loading={false} text="Überprüfen" />
+        <FormButton loading={false} text={t("verify")} />
       </form>
     </div>
   );
